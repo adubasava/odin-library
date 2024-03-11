b004284f70cbfe7f5339d49bfb117b919876fff5
adubasava <anzhalikad@gmail.com>
2024-03-11 17:04:02 +0300

    Create basic structure

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# odin-library
+# Odin Library Project
+
+### Images from:
+
+https://pictogrammers.com/library/mdi/
+
+https://www.pexels.com/photo/light-inside-library-590493/ (photo by Janko Ferlic)
---
+++ script.js
@@ -0,0 +1,26 @@
+﻿const myLibrary = [];
+
+function Book(title, author, genre, numberOfPages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.genre = genre;
+    this.numberOfPages = numberOfPages;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary() {
+    for (let book of myLibrary) {
+        return book;
+    }
+}
+
+
+const theHobbit = new Book('The Hobbit, or There and Back Again', 'J.R.R. Tolkien', 'fantasy', '295', false);
+myLibrary.push(theHobbit);
+
+const hotel = new Book('Hotel', 'A. Hailey', 'novel', '608', true);
+myLibrary.push(hotel);
+
+const sherlock = new Book('The Great Adventures of Sherlock Holmes', 'A. Conan Doyle', 'detective fiction', '276', true);
+myLibrary.push(sherlock);
+
