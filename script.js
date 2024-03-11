17c327d03468407c1d8e110974ee0a78597a18ad
adubasava <anzhalikad@gmail.com>
2024-03-11 21:05:32 +0300

    Add remove and toggle status buttons

--- script.js
+++ script.js
@@ -8,34 +8,41 @@ function Book(title, author, genre, numberOfPages, isRead) {
     this.isRead = isRead;
 }
 
-function addBookToLibrary() {
-    for (let book of myLibrary) {
+function displayBookCards() {
+    document.querySelector('.main-container').textContent = "";
+
+    for (let i = 0, j = myLibrary.length; i < j; i++) {
         
         const newDiv = document.createElement("div");
         newDiv.className = "card";
 
+        const status = myLibrary[i].isRead ? "Read" : "Not read";
+
         newDiv.innerHTML = [
             "<span class=title>",
-            book.title,
+            myLibrary[i].title,
             "</span/>",
             "<br><br>",
             "<span class=author>",
-            book.author,
+            myLibrary[i].author,
             "</span/>",
             "<br><br>",
-            book.genre,
+            myLibrary[i].genre,
             "<br><br>",
-            book.numberOfPages,
+            myLibrary[i].numberOfPages,
             " p.",
             "<br><br>",
-            book.isRead,
-        ].join("")
+            `<button class='status ${myLibrary[i].isRead}' data-id=${i}>${status}</button>`,
+            "<br><br><center>",
+            `<button class='remove ${i}' data-index=${i}>Remove a book</button></center>`,
+        ].join("");        
 
-        document.querySelector('.main-container').appendChild(newDiv);
+        document.querySelector('.main-container').appendChild(newDiv);      
+        
     }
 }
 
-
+// Starting set of books
 const theHobbit = new Book('The Hobbit, or There and Back Again', 'J. R. R. Tolkien', 'fantasy', '295', false);
 myLibrary.push(theHobbit);
 
@@ -45,8 +52,11 @@ myLibrary.push(hotel);
 const sherlock = new Book('The Great Adventures of Sherlock Holmes', 'A. Conan Doyle', 'detective fiction', '276', true);
 myLibrary.push(sherlock);
 
-document.querySelector('#show').addEventListener ('click', addBookToLibrary);
 
+document.querySelector('#show').addEventListener('click', displayBookCards);
+
+
+// Adding a book
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button");
 const closeButton = document.querySelector("dialog button");
@@ -59,4 +69,44 @@ closeButton.addEventListener("click", () => {
   dialog.close();
 });
 
+addEventListener("submit", addBook);
 
+function addBook(event) {
+    event.preventDefault();
+
+    const newBook = new Book(
+        document.querySelector('#title').value, 
+        document.querySelector('#author').value, 
+        document.querySelector('#genre').value,  
+        document.querySelector('#pages').value, 
+        document.querySelector('#isread').checked, 
+        );
+    myLibrary.push(newBook);
+    dialog.close();
+}
+
+// Remove a book from the library
+
+removeButtons = document.querySelectorAll('.remove');
+
+removeButtons.forEach((btn) => {
+    btn.addEventListener('click', removeBook(btn.dataset.index));
+})
+
+function removeBook(id) {
+    myLibrary.splice(id, 1);
+    displayBookCards();
+}
+
+// Toggle status
+
+bookStatus = document.querySelectorAll('.status');
+
+bookStatus.forEach((btn) => {
+    btn.addEventListener('click', toggleStatus(btn.dataset.id));
+})
+
+function toggleStatus(id) {
+    myLibrary[id].isRead = myLibrary[id].isRead ? false : true;    
+    displayBookCards();
+}
