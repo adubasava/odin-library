f7d85ac8fd34a5f193cb6793ad4140922715b308
adubasava <anzhalikad@gmail.com>
2024-03-11 17:59:21 +0300

    Add option Show available books

--- script.js
+++ script.js
@@ -10,12 +10,33 @@ function Book(title, author, genre, numberOfPages, isRead) {
 
 function addBookToLibrary() {
     for (let book of myLibrary) {
-        return book;
+        
+        const newDiv = document.createElement("div");
+        newDiv.className = "card";
+
+        newDiv.innerHTML = [
+            "<span class=title>",
+            book.title,
+            "</span/>",
+            "<br><br>",
+            "<span class=author>",
+            book.author,
+            "</span/>",
+            "<br><br>",
+            book.genre,
+            "<br><br>",
+            book.numberOfPages,
+            " p.",
+            "<br><br>",
+            book.isRead,
+        ].join("")
+
+        document.querySelector('.main-container').appendChild(newDiv);
     }
 }
 
 
-const theHobbit = new Book('The Hobbit, or There and Back Again', 'J.R.R. Tolkien', 'fantasy', '295', false);
+const theHobbit = new Book('The Hobbit, or There and Back Again', 'J. R. R. Tolkien', 'fantasy', '295', false);
 myLibrary.push(theHobbit);
 
 const hotel = new Book('Hotel', 'A. Hailey', 'novel', '608', true);
@@ -24,3 +45,18 @@ myLibrary.push(hotel);
 const sherlock = new Book('The Great Adventures of Sherlock Holmes', 'A. Conan Doyle', 'detective fiction', '276', true);
 myLibrary.push(sherlock);
 
+document.querySelector('#show').addEventListener ('click', addBookToLibrary);
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// "Close" button closes the dialog
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
